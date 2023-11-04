2d37240ef285c6535f76540e016a669104ddfab7
Dinh Trong Nghia <nghiadt@mirabo-global.com>
2023-11-05 01:03:13 +0700

    add product user function API + connect database

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/store', {
+      dbName: 'ecommerce_store',
+    }),
+    ProductModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
